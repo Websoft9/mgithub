d1c99d257900bfb5e6d18fe3de422dfda12d143f
Haozhe Chen <chz_chen@outlook.com>
2021-01-27 18:45:35 +0800

    Update GithubCLI.py & setup.py

--- backup/GithubCLI.py
+++ backup/GithubCLI.py
@@ -1,4 +1,5 @@
 import click
+from GithubCommand import GithubCommand
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -10,9 +11,9 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
               help='set your git URL, the default value is https://github.com/websoft9.',
               default='https://github.com/websoft9')
 @click.option('--skip-get-repositories', help='skip get repositories.', is_flag=True)
-@click.option('--skip-broken', help='skip command error and continue next repository.', is_flag=True)
+@click.option('--skip_broken', help='skip command error and continue next repository.', is_flag=True)
 @click.option('-f', '--force', help='do not prompt before overwriting.',is_flag=True)
-def mgithub():
+def mgithub(version, logs, url, skip_get_repositories, skip_broken, force):
     pass
 
 
@@ -21,3 +22,13 @@ def mgithub():
 def configure(url):
     click.echo("running...")
     click.echo("the url: %s" % (url))
+
+@mgithub.command()
+@click.argument('source_path',nargs=1,required=True)
+@click.argument('destination_path',nargs=1,required=True)
+def copy(source_path, destination_path):
+    click.echo("source: %s" % source_path)
+    click.echo("des: %s" % destination_path)
+    command = GithubCommand()
+    command.copy(source_path, destination_path)
+
