f9f38acd0d53400ab47b1750fe042ff7a3322980
Haozhe Chen <chz_chen@outlook.com>
2021-01-27 18:46:04 +0800

    Update GithubCommand.py

---
+++ backup/GithubCommand.py
@@ -0,0 +1,6 @@
+class GithubCommand:
+
+    def copy(self, src_path, des_path):
+        print('GithubCommand.copy is running')
+        print('src_path: %s' % src_path)
+        print('des_path: %s' % des_path)
