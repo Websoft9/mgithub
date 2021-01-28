c408bb8c8610bb58362e7614f8787550c85df40c
Haozhe Chen <chz_chen@outlook.com>
2021-01-28 14:07:30 +0800

    Finish basic CLI framework

--- backup/GithubCLI.py
+++ backup/GithubCLI.py
@@ -1,25 +1,27 @@
 import click
+import os
 from GithubCommand import GithubCommand
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 command = GithubCommand()
 
+
 @click.group(context_settings=CONTEXT_SETTINGS, invoke_without_command=True)
 @click.option('-v', '--version', help='show the version.', is_flag=True)
 @click.option('-l', '--logs', help='show the logs.', is_flag=True)
-@click.option('-url',
-              help='set your git URL, the default value is https://github.com/websoft9.',
-              prompt="url: ")
 @click.option('--skip-get-repositories', help='skip get repositories.', is_flag=True)
 @click.option('--skip-broken', help='skip command error and continue next repository.', is_flag=True)
 @click.option('-f', '--force', help='do not prompt before overwriting.', is_flag=True)
 @click.pass_context
-def mgithub(ctx, version, logs, url, skip_get_repositories, skip_broken, force):
+def mgithub(ctx, version, logs, skip_get_repositories, skip_broken, force):
+    if ctx.invoked_subcommand is None:
+        os.system("mgithub -h")
+
     ctx.ensure_object(dict)
     ctx.obj['version'] = version
     ctx.obj['logs'] = logs
-    ctx.obj['url'] = url
+    ctx.obj['url'] = "https://github.com/websoft9"
     ctx.obj['skip_get_repositories'] = skip_get_repositories
     ctx.obj['skip_broken'] = skip_broken
     ctx.obj['force'] = force
@@ -30,24 +32,15 @@ def mgithub(ctx, version, logs, url, skip_get_repositories, skip_broken, force):
     if logs:
         click.echo("Showing logs ....")
 
-    print("url is %s" % url)
-    if url is not None:
-        click.echo("User setting URL ....")
 
-    #
-    # click.echo("################ option flag check: ")
-    # click.echo("--version: %s" % version)
-    # click.echo("--logs: %s" % logs)
-    # click.echo("--url: %s" % url)
-    # click.echo("--sgr: %s" % skip_get_repositories)
-    # click.echo("--sb: %s" % skip_broken)
-    # click.echo("--force: %s" % force)
+@mgithub.command()
+@click.pass_context
+@click.option('-url', nargs=1, prompt="input URL",
+              help="Set your git URL, the default value is https://github.com/websoft9.")
+def configure(ctx, url):
+    ctx.obj['url'] = url
+    command.configure(ctx)
 
-
-# @mgithub.command()
-# @click.option('-url', required=True, prompt=True)
-# def configure(url):
-#     click.echo("input url: ")
 
 @mgithub.command()
 @click.pass_context
@@ -57,7 +50,70 @@ def repocache(ctx):
 
 @mgithub.command()
 @click.pass_context
+@click.argument('path', nargs=1, required=True)
+def backup(ctx, path):
+    command.backup(ctx, path)
+
+
+@mgithub.command()
+@click.pass_context
 @click.argument('source_path', nargs=1, required=True)
 @click.argument('destination_path', nargs=1, required=True)
 def copy(ctx, source_path, destination_path):
-    command.copy(source_path, destination_path)
+    command.copy(ctx, source_path, destination_path)
+
+
+@mgithub.command()
+@click.pass_context
+@click.argument('source_path', nargs=1, required=True)
+@click.argument('destination_path', nargs=1, required=True)
+def move(ctx, source_path, destination_path):
+    command.move(ctx, source_path, destination_path)
+
+
+@mgithub.command()
+@click.pass_context
+@click.argument('path', nargs=1, required=True)
+def delete(ctx, path):
+    command.delete(ctx, path)
+
+
+@mgithub.command()
+@click.pass_context
+@click.argument('path', nargs=1, required=True)
+@click.argument('new_name', nargs=1, required=True)
+def rename(ctx, path, new_name):
+    command.rename(ctx, path, new_name)
+
+
+@mgithub.command()
+@click.pass_context
+@click.argument('file_path', nargs=1, required=True)
+@click.argument('old_content', nargs=1, required=True)
+@click.argument('new_content', nargs=1, required=False)
+def replace(ctx, file_path, old_content, new_content):
+    command.replace(ctx, file_path, old_content, new_content)
+
+
+@mgithub.command()
+@click.pass_context
+@click.argument('file_path', nargs=1, required=True)
+@click.argument('line', nargs=-1, required=True)
+@click.argument('content', nargs=1, required=True)
+def lineinsert(ctx, file_path, line, content):
+    command.lineinsert(ctx, file_path, line, content)
+
+
+@mgithub.command()
+@click.pass_context
+@click.argument('template', nargs=1, required=True)
+@click.argument('variable', nargs=1, required=True)
+def format(ctx, template, variable):
+    command.format(ctx, template, variable)
+
+
+@mgithub.command()
+@click.pass_context
+@click.argument('clistring', nargs=1, required=True)
+def githubcli(ctx, clistring):
+    command.githubcli(ctx, clistring)
--- backup/GithubCommand.py
+++ backup/GithubCommand.py
@@ -1,14 +1,76 @@
 class GithubCommand:
 
+    def configure(self, ctx):
+        print("[[configure]] function is running")
+        GithubCommand.debug(ctx)
+
     def repocache(self, ctx):
-        print("repocache function is running")
-        print("########## flag check ##########")
+        print("[[repocache]] function is running")
+        GithubCommand.debug(ctx)
+
+    def backup(self, ctx, path):
+        print("[[backup]] function is running")
+        print("path is: %s" % path)
+        GithubCommand.debug(ctx)
+
+    def copy(self, ctx, src_path, des_path):
+        print("[[backup]] function is running")
+        print('src_path: %s' % src_path)
+        print('des_path: %s' % des_path)
+        GithubCommand.debug(ctx)
+
+    def move(self, ctx, src_path, des_path):
+        print("[[move]] function is running")
+        print('src_path: %s' % src_path)
+        print('des_path: %s' % des_path)
+        GithubCommand.debug(ctx)
+
+    def delete(self, ctx, path):
+        print("[[delete]] function is running")
+        print("path is: %s" % path)
+        GithubCommand.debug(ctx)
+
+    def rename(self, ctx, path, new_name):
+        print("[[rename]] function is running")
+        print("path is: %s" % path)
+        print("new_name is: %s" % new_name)
+        GithubCommand.debug(ctx)
+
+    def replace(self, ctx, file_path, old_content, new_content):
+        print("[[replace]] function is running")
+        print("file_path is: %s" % file_path)
+        print("old_content is: %s" % old_content)
+        print("new_content is: %s" % new_content)
+        if (new_content == None):
+            print("Since not new_content is provided, old_content will be deleted from file_path")
+        GithubCommand.debug(ctx)
+
+    def lineinsert(self, ctx, file_path, line, content):
+        print("[[lineinsert]] function is running")
+        print("file_path is: %s" % file_path)
+        print("line is: %s" % (line, ))
+        print("content is: %s" % content)
+        for li in line:
+            print("insert %s into line %s of %s" % (content, li, file_path))
+        GithubCommand.debug(ctx)
+
+    def format(self, ctx, template, variable):
+        print("[[format]] function is running")
+        print("template is: %s" % template)
+        print("variable is: %s" % variable)
+        GithubCommand.debug(ctx)
+
+    def githubcli(self, ctx, clistring):
+        print("[[githubcli]] function is running")
+        print("clistring is: %s" % clistring)
+        print("printing log ....")
+        GithubCommand.debug(ctx)
+
+    @staticmethod
+    def debug(ctx):
+        print("########## debug flag check ##########")
+        print("url: %s" % ctx.obj['url'])
         print("skip-get-repositories: %s" % ctx.obj['skip_get_repositories'])
         print("skip-broken: %s" % ctx.obj['skip_broken'])
         print("force: %s" % ctx.obj['force'])
 
-
-    def copy(self, src_path, des_path):
-        print('GithubCommand.copy is running')
-        print('src_path: %s' % src_path)
-        print('des_path: %s' % des_path)
