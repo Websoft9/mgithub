2caf1bc420263ac2a7d123db9e12aabfd4aacbd6
Haozhe Chen <chz_chen@outlook.com>
2021-01-28 12:17:50 +0800

    Update GithubCLI & GithubCommand

--- backup/GithubCLI.py
+++ backup/GithubCLI.py
@@ -3,34 +3,19 @@ from GithubCommand import GithubCommand
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
-# class option:
-#     version = False
-#     logs = False
-#     url = "https://github.com/websoft"
-#     skip_get_repositories = False
-#     skip_broken = False
-#     force = False
-#
-#     def __init__(self, version, logs, url, skip_get_repositories, skip_broken, force):
-#         self.version = version
-#         self.logs = logs
-#         self.url = url
-#         self.skip_get_repositories = skip_get_repositories
-#         self.skip_broken = skip_broken
-#         self.force = force
+command = GithubCommand()
 
 @click.group(context_settings=CONTEXT_SETTINGS, invoke_without_command=True)
 @click.option('-v', '--version', help='show the version.', is_flag=True)
 @click.option('-l', '--logs', help='show the logs.', is_flag=True)
 @click.option('-url',
               help='set your git URL, the default value is https://github.com/websoft9.',
-              default='https://github.com/websoft9')
+              prompt="url: ")
 @click.option('--skip-get-repositories', help='skip get repositories.', is_flag=True)
 @click.option('--skip-broken', help='skip command error and continue next repository.', is_flag=True)
 @click.option('-f', '--force', help='do not prompt before overwriting.', is_flag=True)
 @click.pass_context
 def mgithub(ctx, version, logs, url, skip_get_repositories, skip_broken, force):
-
     ctx.ensure_object(dict)
     ctx.obj['version'] = version
     ctx.obj['logs'] = logs
@@ -40,34 +25,39 @@ def mgithub(ctx, version, logs, url, skip_get_repositories, skip_broken, force):
     ctx.obj['force'] = force
 
     if version:
-        click.echo("version is true")
+        click.echo("mgithub version 1.0.1")
 
-    click.echo("################ option flag check: ")
-    click.echo("--version: %s" % version)
-    click.echo("--logs: %s" % logs)
-    click.echo("--url: %s" % url)
-    click.echo("--sgr: %s" % skip_get_repositories)
-    click.echo("--sb: %s" % skip_broken)
-    click.echo("--force: %s" % force)
+    if logs:
+        click.echo("Showing logs ....")
 
-@click.option('-v', '--version', help='show the version.', is_flag=True)
-@click.command()
-def version(ctx, version):
-    click.echo("Version 0.1")
+    print("url is %s" % url)
+    if url is not None:
+        click.echo("User setting URL ....")
+
+    #
+    # click.echo("################ option flag check: ")
+    # click.echo("--version: %s" % version)
+    # click.echo("--logs: %s" % logs)
+    # click.echo("--url: %s" % url)
+    # click.echo("--sgr: %s" % skip_get_repositories)
+    # click.echo("--sb: %s" % skip_broken)
+    # click.echo("--force: %s" % force)
+
+
+# @mgithub.command()
+# @click.option('-url', required=True, prompt=True)
+# def configure(url):
+#     click.echo("input url: ")
+
+@mgithub.command()
+@click.pass_context
+def repocache(ctx):
+    command.repocache(ctx)
+
 
 @mgithub.command()
 @click.pass_context
 @click.argument('source_path', nargs=1, required=True)
 @click.argument('destination_path', nargs=1, required=True)
 def copy(ctx, source_path, destination_path):
-    click.echo("source: %s" % source_path)
-    click.echo("des: %s" % destination_path)
-    command = GithubCommand()
     command.copy(source_path, destination_path)
-    click.echo("################ option flag check: ")
-    click.echo("--version: %s" % ctx.obj['version'])
-    click.echo("--logs: %s" % ctx.obj['logs'])
-    click.echo("--url: %s" % ctx.obj['url'])
-    click.echo("--sgr: %s" % ctx.obj['skip_get_repositories'])
-    click.echo("--sb: %s" % ctx.obj['skip_broken'])
-    click.echo("--force: %s" % ctx.obj['force'])
--- backup/GithubCommand.py
+++ backup/GithubCommand.py
@@ -1,5 +1,13 @@
 class GithubCommand:
 
+    def repocache(self, ctx):
+        print("repocache function is running")
+        print("########## flag check ##########")
+        print("skip-get-repositories: %s" % ctx.obj['skip_get_repositories'])
+        print("skip-broken: %s" % ctx.obj['skip_broken'])
+        print("force: %s" % ctx.obj['force'])
+
+
     def copy(self, src_path, des_path):
         print('GithubCommand.copy is running')
         print('src_path: %s' % src_path)
