924402ad972d7b7a78c55f753ef6e3bd7d615645
Haozhe Chen <chz_chen@outlook.com>
2021-02-02 19:30:54 +0800

    Refactor and Comment

--- GithubFlow.py
+++ GithubFlow.py
@@ -21,64 +21,75 @@ class GithubFlow():
         self.repo_str = None
         self.current_proj = None
 
+    # 添加对Ctrl-C的监听器
     def signal_handler(self, signal, frame):
         if os.path.isfile(self.repo_str):
             product = GithubProduct(self.url, self.skip_get_repo, self.skip_broken, self.force, self.product_kind,
                                     self.src_path,
                                     self.des_path, self.repo_str)
             if self.current_proj == None:
+                # 如果当前还没有开始执行项目，则任务项目列表中的第一个项目执行失败
                 product.complete_work(2, open(self.repo_str).read().splitlines()[0])
             else:
+                # 如果已经开始执行项目，则当前项目执行失败
                 product.complete_work(2, self.current_proj)
         print("用户主动中断任务")
         sys.exit(0)
 
-    # 根据组织确定需要作业的范围对象,对所有对象循环处理要做的自动化处理
+    # 对本组织下的仓库进行循环处理任务
     def auto_make(self):
 
+        # 将监听器注册到本function
         signal.signal(signal.SIGINT, self.signal_handler)
 
         print("============================ Automake task starting... ============================ \n")
 
+        # 判断项目列表是否存在
         self.repo_str = "data/" + self.organization + "_repositories.txt"
-
-        # 判断缓存文件是否存在
         if os.path.isfile(self.repo_str):
+            # 项目列表存在，判断内容是否为空
             project_list = open(self.repo_str).read().splitlines()
             if (len(project_list) == 0):
+                # 内容为空，直接执行正常操作
                 self.automake_new()
             else:
-                print("清单里有未完成的任务列表, 请确认对列表内容")
+                # 内容不为空，让用户决定是否继续上次的任务
+                print("清单里有未完成的任务列表, 请确认列表内容")
                 print("未完成清单内容如下：")
                 GithubTools.execute_CmdCommand("cat " + self.repo_str)
                 print("最新10条任务日志如下：")
                 GithubTools.execute_CmdCommand("tail -n 10 log/auto_make.log")
-
+                # 用户输入
                 continue_id = self.continue_select()
-
+                # 根据不同的用户输入决定操作
                 if continue_id == "0":
+                    # 继续上次中断的任务
                     self.automake_cache()
                     return
                 else:
+                    # 放弃上次的任务并删除项目列表
                     print("已经清空所有未完成任务，请重新输入命令再次执行")
                     GithubTools.execute_CmdCommand(": > data/" + self.organization + "_repositories.txt")
                     return
         else:
             # 缓存文件不存在，创建空的缓存文件
             GithubTools.execute_CommandReturn('touch ' + self.repo_str)
+            # 执行正常操作
             self.automake_new()
 
+    # 无任务中断，正常操作
     def automake_new(self):
 
+        # 创建一个新的GitHubProduct对象并传入全局参数与command参数
         product = GithubProduct(self.url, self.skip_get_repo, self.skip_broken, self.force, self.product_kind,
                                 self.src_path,
                                 self.des_path, self.repo_str)
 
-        # 获取组织下的项目列表并写入相应的xxxx_repositories.txt
+        # 获取组织下的所有项目并写入项目列表
         self.create_repository(self.organization, self.repo_str)
         project_list = open(self.repo_str).read().splitlines()
 
-        # 如果项目列表为空，可能由于网络原因无法获取
+        # 如果项目列表为空，可能由于网络原因无法获取或者组织名错误
         try:
             if len(project_list) == 0:
                 raise CustomException("您的仓库列表为空,请检查您的网络连接或组织名称")
@@ -87,16 +98,78 @@ class GithubFlow():
             print("============================ [[Empty]]: 任务列表为空\n")
             return
 
+        # 根据项目列表，将每个项目的远程仓库clone到本地
+        self.clone_repo_list(project_list, product)
+
+        # 根据项目列表，对每个项目进行循环command操作
+        self.loop_proj_work(project_list, product)
+
+    # 执行上次中断的任务
+    def automake_cache(self):
+
+        # 获得上次遗留的项目列表
+        project_list = open(self.repo_str).read().splitlines()
+
+        # 获得log日志列表
+        log_list = open("log/auto_make.log").read().splitlines()
+
+        # 根据遗留项目列表的第一项，寻找log日志列表中相对应的 FAILED 或 ABORT 项目（自下而上）
+        log_index = len(log_list) - 1
+        while (log_index >= 0):
+
+            if (log_list[log_index].split("|")[5] == project_list[0] and
+                    (log_list[log_index].split("|")[1] == "FAILED" or
+                     log_list[log_index].split("|")[1] == "ABORT")):
+                # 根据日志记录的option内容，对本次任务的option进行覆盖
+                self.product_kind = log_list[log_index].split("|")[7].lower()
+                self.organization = log_list[log_index].split("|")[3]
+                self.src_path = log_list[log_index].split("|")[9]
+                self.des_path = log_list[log_index].split("|")[11]
+                self.url = log_list[log_index].split("|")[19]
+                self.skip_get_repo = log_list[log_index].split("|")[15]
+                self.skip_broken = log_list[log_index].split("|")[17]
+                self.force = log_list[log_index].split("|")[13]
+
+                # 生成一个新的GitHubProduct对象
+                product = GithubProduct(self.url, self.skip_get_repo, self.skip_broken, self.force, self.product_kind,
+                                        self.src_path,
+                                        self.des_path, self.repo_str)
+
+                # 根据项目列表，将每个项目的远程仓库clone到本地
+                self.clone_repo_list(project_list, product)
+
+                # 根据项目列表，对每个项目进行循环command操作
+                self.loop_proj_work(project_list, product)
+
+                # 中断循环
+                break
+
+            # 从下而上检索log日志条例
+            log_index -= 1
+
+    # 根据组织寻找组织下所有的项目
+    def create_repository(self, organization, repository_str):
+        print('开始更新%s Github仓库列表...' % organization)
+        GithubTools.execute_CommandWriteFile(
+            'curl -s  https://api.github.com/orgs/mgithubTestOrg/repos?per_page=999999 | grep \'"name"\'|awk -F \'"\' \'{print $4}\'',
+            repository_str)
+
+    # 根据项目列表，将每个项目的远程仓库clone到本地
+    def clone_repo_list(self, project_list, product):
+
+        # 如果用户执行时使用 mgithub --skip-get-repositories
         if self.skip_get_repo:
-            # mgithub --skip-get-repositories
             print("\n已跳过clone仓库步骤, 本地已有的仓库将会在执行过程中更新")
         else:
             print("\n正在将本组织下的仓库clone到本地...")
             for proj in project_list:
                 FILE_PATH = "data/" + self.organization + "/" + proj
+                # 判断仓库是否已经保存在本地
                 if os.path.isdir(FILE_PATH):
+                    # 存在
                     print(FILE_PATH + "已存在, 可以使用option: --skip-get-repositories 跳过本步骤")
                 else:
+                    # 不存在，从远程仓库clone
                     print("git clone from " + proj + "....")
                     cmd = "git clone --depth=1 " + self.url + "/" + proj + ".git data/" + self.organization + "/" + proj
                     try:
@@ -108,108 +181,35 @@ class GithubFlow():
                         product.complete_work(0, proj)
                         return
 
-        # 对项目列表中的项目进行循环执行操作
+    # 根据项目列表，对每个项目进行循环command操作
+    def loop_proj_work(self, project_list, product):
         for proj in project_list:
+            # 记录本次正在执行command的项目，方便与监听器记录
             self.current_proj = proj
+            # 执行相应的command
             try:
                 product.product_execute(proj)
             except CustomException as e:
-                # 操作抛出异常
+                # 捕捉执行过程中出现异常
                 print(e.msg)
+                # 已 FAILED 记录本次任务
                 product.complete_work(0, proj)
+                # 对项目进行回滚
                 print("============================ [[" + proj + "]]: 项目正在回滚")
-                # 回滚项目
                 cmd = "cd data/" + self.organization + "/" + proj + ";git fetch --all;git reset --hard origin/master;git clean -f -d;"
                 try:
                     GithubTools.execute_CommandIgnoreReturn(cmd)
                 except CustomException as e:
+                    # 项目回滚中出现异常，回滚失败
                     print(e.msg)
                     print(proj + ": 项目回滚失败, 请检查您的网络连接状况和组织名称")
                 else:
                     print(proj + ": 项目已回滚")
+                # 如果用户使用 mgithub --skip-broken
                 if self.skip_broken:
                     continue
                 else:
                     return
-
-    def automake_cache(self):
-
-        project_list = open(self.repo_str).read().splitlines()
-        log_list = open("log/auto_make.log").read().splitlines()
-        log_index = len(log_list) - 1
-        while (log_index >= 0):
-            print(log_list[log_index].split("|")[1])
-            print(project_list[0])
-            if (log_list[log_index].split("|")[5] == project_list[0] and
-                (log_list[log_index].split("|")[1] == "FAILED" or
-                 log_list[log_index].split("|")[1] == "ABORT")):
-                # print(log_list[log_index])
-
-                self.product_kind = log_list[log_index].split("|")[7].lower()
-                self.organization = log_list[log_index].split("|")[3]
-                self.src_path = log_list[log_index].split("|")[9]
-                self.des_path = log_list[log_index].split("|")[11]
-
-                self.url = log_list[log_index].split("|")[19]
-                self.skip_get_repo = log_list[log_index].split("|")[15]
-                self.skip_broken = log_list[log_index].split("|")[17]
-                self.force = log_list[log_index].split("|")[13]
-
-                product = GithubProduct(self.url, self.skip_get_repo, self.skip_broken, self.force, self.product_kind,
-                                self.src_path,
-                                self.des_path, self.repo_str)
-
-                if self.skip_get_repo == "True":
-                    # mgithub --skip-get-repositories
-                    print("\n已跳过clone仓库步骤, 本地已有的仓库将会在执行过程中更新")
-                else:
-                    print("\n正在将本组织下的仓库clone到本地...")
-                    for proj in project_list:
-                        FILE_PATH = "data/" + self.organization + "/" + proj
-                        if os.path.isdir(FILE_PATH):
-                            print(FILE_PATH + "已存在, 可以使用option: --skip-get-repositories 跳过本步骤")
-                        else:
-                            print("git clone from " + proj + "....")
-                            cmd = "git clone --depth=1 " + self.url + "/" + proj + ".git data/" + self.organization + "/" + proj
-                            try:
-                                GithubTools.execute_CommandIgnoreReturn(cmd)
-                            except CustomException as e:
-                                # 仓库clone未成功，结束本次任务
-                                print(e.msg)
-                                print("仓库clone未成功，结束本次任务")
-                                product.complete_work(0, proj)
-                                return
-
-                for proj in project_list:
-                    self.current_proj = proj
-                    try:
-                        product.product_execute(proj)
-                    except CustomException as e:
-                        # 操作抛出异常
-                        print(e.msg)
-                        product.complete_work(0, proj)
-                        print("============================ [[" + proj + "]]: 项目正在回滚")
-                        # 回滚项目
-                        cmd = "git fetch --all;git reset --hard origin/master;git clean -f -d"
-                        try:
-                            GithubTools.execute_CommandIgnoreReturn(cmd)
-                        except CustomException as e:
-                            print(e.msg)
-                            print(proj + ": 项目回滚失败, 请检查您的网络连接状况和组织名称")
-                        print(proj + ": 项目已回滚")
-                        if self.skip_broken == "True":
-                            continue
-                        else:
-                            return
-                break
-            log_index -= 1
-
-    # 根据organization生成最新的repository文件列表
-    def create_repository(self, organization, repository_str):
-        print('开始更新%s Github仓库列表...' % organization)
-        GithubTools.execute_CommandWriteFile(
-            'curl -s  https://api.github.com/orgs/mgithubTestOrg/repos?per_page=999999 | grep \'"name"\'|awk -F \'"\' \'{print $4}\'',
-            repository_str)
 
     # 选择继续操作
     # 格式：选择是否在上次任务上继续：  \n\t 0. 继续 \n\t 1. 终止退出  \n\n\t选择:
@@ -221,6 +221,7 @@ class GithubFlow():
             input_str = input_str + str(i) + "." + x + " \n\t "
             i = i + 1
         input_str = input_str + "\n\t选择: "
+        
         continue_id = input(input_str)
         while continue_id not in ['0', '1']:
             print('\n\t输入错误，请重新选择')
--- GithubProduct.py
+++ GithubProduct.py
@@ -18,30 +18,35 @@ class GithubProduct():
         self.organization = url.split("/")[len(url.split("/")) - 1]
         self.repo_str = repo_str
 
+
     # 执行自动化命令
     def product_execute(self, project):
+
         print("\n============================ [[" + project + "]]: 开始执行自动化构建")
 
+        # 对本地仓库进行更新
         print("更新本地仓库: " + project)
         FILE_PATH = "data/" + self.organization + "/" + project
+        # 判断本地仓库是否已经存在
         if os.path.isdir(FILE_PATH):
-            # 更新本地的仓库列表
+            # 存在：使用git pull对本地仓库进行更新
             cmd = "cd " + FILE_PATH + "; git pull"
             try:
                 GithubTools.execute_CommandIgnoreReturn(cmd)
             except CustomException as e:
                 raise e
         else:
-            # clone仓库列表
+            # 不存在：使用git clone从新获取本地仓库
             cmd = "git clone --depth=1 " + self.url + "/" + project + ".git data/" + self.organization + "/" + project
             try:
                 GithubTools.execute_CommandIgnoreReturn(cmd)
             except CustomException as e:
                 raise e
 
+        # COPY
         if self.product_kind == "copy":
+            # 判断是否需要强制覆盖文件
             if self.force:
-                # mgithub --force
                 print("\n执行强制覆盖的copy动作...")
                 cmd = "cp -rf data/" + self.organization + "/" + project + "/" + self.src_path + " data/" + self.organization + "/" + project + "/" + self.des_path
             else:
@@ -49,22 +54,36 @@ class GithubProduct():
                 cmd = "awk \'BEGIN { cmd=\"cp -ri %s %s\"; print \"n\" |cmd; }\'" % (
                     "data/" + self.organization + "/" + project + "/" + self.src_path,
                     "data/" + self.organization + "/" + project + "/" + self.des_path,)
+            # 执行相应的COPY命令
             try:
                 GithubTools.execute_CmdCommand(cmd)
             except CustomException as e:
                 raise e
 
-            print("\n正在将本地改动push到远程仓库...")
-            try:
-                self.github_push(project)
-            except CustomException as e:
-                raise e
+        elif self.productkind == "delete" :
+            pass
+        elif self.productkind == "modify" :
+            pass
+        elif self.productkind == "format" :
+            pass
+        elif self.productkind == "branch" :
+            pass
+        elif self.productkind == "backup" :
+            pass
 
-            self.complete_work(1, project)
+        # 将本地改动PUSH到远程仓库
+        print("\n正在将本地改动push到远程仓库...")
+        try:
+            self.github_push(project)
+        except CustomException as e:
+            raise e
+
+        # 本次任务完成
+        self.complete_work(1, project)
+
 
     # 将本地工程提交到github（push to remote）
     def github_push(self, project):
-
         print(project + ": 将本地工程提交到github")
         cmd = 'cd data/%s/%s;\ngit add -A;\ngit commit -m "%s";\ngit push' % (self.organization, project, self.product_kind)
         try:
@@ -72,28 +91,38 @@ class GithubProduct():
         except CustomException as e:
             raise e
 
-    # 主体构建工作完成后的处理，删除表中该工程
+
+    # 主体构建工作完成后的处理
     def complete_work(self, flag, project):
+        # 由flag参数判断本次操作是否成功
         if flag == 1:
+            # 操作成功
             print("\n" + project + ": 自动化任务完成,从缓存列表删除该工程,并追加日志")
+            # 删除列表中对应的项目
             cmd = "sed -i '""' '/^$/d;/" + project + "/d' " + self.repo_str
             GithubTools.execute_CommandIgnoreReturn(cmd)
+            # 生成log
             self.log_maker(project, flag)
             print("============================ [[" + project + "]]: 本项目任务成功\n")
         else:
+            # 操作失败
             print("\n" + project + ": 自动化任务未完成,在缓存列表保留此工程,并追加日志")
+            # 生成log
             self.log_maker(project, flag)
             print("============================ [[" + project + "]]: 本项目任务失败\n")
 
+
+    # 生成log
     def log_maker(self, project, flag):
         FILE_PATH = "log/auto_make.log"
         nowtime = time.strftime("%H:%M:%S")
         logline = nowtime
-        if (flag == 1):
+        #对任务结束类型进行判断
+        if flag == 1:
             logline += " |OK"
-        elif (flag == 0):
+        elif flag == 0:
             logline += " |FAILED"
-        elif (flag == 2):
+        elif flag == 2:
             logline += " |ABORT"
         logline += "| organization: |" + self.organization + "| project: |" \
                    + project + "| execute " + "|" + self.product_kind.upper() \
