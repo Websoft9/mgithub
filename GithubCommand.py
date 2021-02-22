76974009e5893ef8a8e568564a0b060e1187250f
Haozhe Chen <chz_chen@outlook.com>
2021-02-22 13:36:28 +0800

    Solving repocache bugs

--- GithubCommand.py
+++ GithubCommand.py
@@ -20,24 +20,35 @@ class GithubCommand:
 
     # 功能：Generate the repositories cache
     def repocache(self, ctx):
+
         # 通过url得到organization name
         organization = ctx.obj["url"].split("/")[len(ctx.obj["url"].split("/")) - 1]
         print('开始更新%s Github仓库列表...' % organization)
+
         # 清除本地已存在的项目列表
         GithubSystem.execute_CmdCommand(": > data/" + organization + "_repositories.txt")
-        # 从github official api获取用户/个人的所用项目信息并存为json
-        GithubSystem.execute_Command(
-            'curl -s  https://api.github.com/users/' + organization + "/repos > data/repoapi.json"
-        )
-        # json -> python data
-        with open('data/repoapi.json', 'r') as f:
-            dict = json.load(f)
-        # 对json中的每一个仓库信息进行遍历，找到仓库名并写入项目列表
-        for repo in dict:
+
+        page = 1
+        dict = []
+        while len(dict) != 0 or page == 1:
+            # 从github official api获取用户/个人的所用项目信息并存为json
             GithubSystem.execute_Command(
-                "echo " + repo['name'] + " >> data/" + organization + "_repositories.txt"
+                'curl -s  https://api.github.com/users/' + organization + "/repos\?per_page\=100\&page\=" + str(page) + "  > data/repoapi.json"
             )
-            print(repo['name'])
+
+            # json -> python data
+            with open('data/repoapi.json', 'r') as f:
+                dict = json.load(f)
+
+            # 对json中的每一个仓库信息进行遍历，找到仓库名并写入项目列表
+            for repo in dict:
+                GithubSystem.execute_Command(
+                    "echo " + repo['name'] + " >> data/" + organization + "_repositories.txt"
+                )
+                print(repo['name'])
+
+            page += 1
+
         # 如果仓库列表为空
         if not os.path.getsize("data/" + organization + "_repositories.txt"):
             print("仓库列表为空，请检查您的组织/用户名或网络设置")
--- GithubProduct.py
+++ GithubProduct.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 # coding=utf-8
-import os
-import time
-
-from backup.GithubTools import GithubTools
 from GithubException import CustomException
 from GithubSystem import GithubSystem
 
